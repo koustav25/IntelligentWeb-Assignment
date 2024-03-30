64138630a53805b5072efedb1bddc254ef5fb5a9
Benjamin Lister <48570319+007spy9@users.noreply.github.com>
2024-03-30 18:51:51 +0000

    IWP-32 | Created Socket.io events for the plant_details page

---
+++ solution/controllers/socket/plant_details.js
@@ -0,0 +1,39 @@
+function registerPlantDetailsSockets(io, socket) {
+    socket.on('viewing_plant', (data) => {
+        console.log('User viewing plant: ' + data.plant_id)
+        socket.join(data.plant_id);
+    });
+
+    socket.on('leaving_plant', (data) => {
+        console.log('User leaving plant: ' + data.plant_id)
+        socket.leave(data.plant_id);
+    });
+
+    socket.on('new_suggestion', (room, data) => {
+        io.sockets.in(room).emit('new_suggestion', data);
+    });
+
+    socket.on('upvote_count', (room, data) => {
+        io.sockets.in(room).emit('upvote_count', data);
+    });
+
+    socket.on('downvote_count', (room, data) => {
+        io.sockets.in(room).emit('downvote_count', data);
+    });
+
+    socket.on('new_comment', (room, data) => {
+        io.sockets.in(room).emit('new_comment', data);
+    });
+
+    socket.on('new_reply', (room, data) => {
+        io.sockets.in(room).emit('new_reply', data);
+    });
+
+    socket.on('like_count', (room, data) => {
+        io.sockets.in(room).emit('like_count', data);
+    });
+}
+
+module.exports = {
+    registerPlantDetailsSockets
+}
--- solution/public/javascripts/posts/plant_details.js
+++ solution/public/javascripts/posts/plant_details.js
@@ -1,8 +1,11 @@
 let map;
 let postMarker;
 
-document.addEventListener('DOMContentLoaded', function() {
+let socket = io();
+
+document.addEventListener('DOMContentLoaded', function () {
     //TODO: Initialise Socket.io
+    registerSocketListeners();
 
     map = L.map('map').setView([postCoords.latitude, postCoords.longitude], 13);
 
@@ -13,4 +16,44 @@ document.addEventListener('DOMContentLoaded', function() {
 
     postMarker = L.marker([postCoords.latitude, postCoords.longitude]).addTo(map);
 });
+
+function registerSocketListeners() {
+    socket.on('new_suggestion', (data) => {
+
+    });
+
+    socket.on('upvote_count', (data) => {
+
+    });
+
+    socket.on('downvote_count', (data) => {
+
+    });
+
+    socket.on('new_comment', (data) => {
+
+    });
+
+    socket.on('new_reply', (data) => {
+
+    });
+
+    socket.on('like_count', (data) => {
+
+    });
+
+    socket.on('connect', () => {
+        console.log('Connected to server');
+        socket.emit('viewing_plant', {plant_id: plantID});
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected from server');
+        socket.emit('leaving_plant', {plant_id: plantID});
+    });
+
+    window.addEventListener('beforeunload', function (event) {
+        socket.emit('leaving_plant', {plant_id: plantID});
+    });
+}
 
