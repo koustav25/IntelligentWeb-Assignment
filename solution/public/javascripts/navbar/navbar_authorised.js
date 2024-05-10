cb28ec783fb22f5f8b562621195344fc11750ffe
Benjamin Lister <48570319+007spy9@users.noreply.github.com>
2024-05-10 16:46:07 +0100

    IWP-55 | Hidden notification count if count is zero. Tweaked navbar

--- solution/public/javascripts/navbar/navbar_authorised.js
+++ solution/public/javascripts/navbar/navbar_authorised.js
@@ -1,4 +1,4 @@
-let socket = io()
+let socket;
 const $notificationCounter = $("#notification-count")
 
 window.addEventListener("load", async (e) => {
@@ -6,15 +6,27 @@ window.addEventListener("load", async (e) => {
     try {
         const notificationCount = await axios.get("/api/new-notification-count")
         const countInt = parseInt(notificationCount.data.count)
-        $notificationCounter.text(countInt >= 0 ?  countInt : 0)
+        $notificationCounter.text(countInt >= 0 ? countInt : 0)
+
+        if (countInt > 0) {
+            $notificationCounter.removeClass("d-none")
+        }
+
         socket.emit("user_active", {user_id: notificationCount.data.user_id})
 
         socket.on("new_notification", () => {
+            //If the notifications count was 0, then we need to show the counter
+            if (parseInt($notificationCounter.text()) === 0) {
+                $notificationCounter.removeClass("d-none")
+            }
+
             $notificationCounter.text(parseInt($notificationCounter.text()) + 1)
         })
-    }catch(e) {
+    } catch (e) {
         console.log(e)
     }
-
-
 })
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+    socket = io()
+});
--- solution/public/sw.js
+++ solution/public/sw.js
@@ -23,8 +23,7 @@ self.addEventListener('install', event => {
                 '/images/logo.png',
             ]);
             console.log('Service Worker: App Shell Cached');
-        }
-        catch{
+        } catch {
             console.log("error occurred while caching...")
         }
 
@@ -38,8 +37,8 @@ self.addEventListener('activate', event => {
         (async () => {
             const keys = await caches.keys();
             return keys.map(async (cache) => {
-                if(cache !== "static") {
-                    console.log('Service Worker: Removing old cache: '+cache);
+                if (cache !== "static") {
+                    console.log('Service Worker: Removing old cache: ' + cache);
                     return await caches.delete(cache);
                 }
             })
@@ -47,16 +46,38 @@ self.addEventListener('activate', event => {
     )
 })
 
+function shouldBeCached(request) {
+    //Check if the request is a GET request
+    if (request.method !== "GET") {
+        return false;
+    }
+
+    const invalidURLParts =
+        [
+            'api',
+            'socket.io',
+        ];
+
+    //Check if the request URL contains any of the invalid URL parts
+    for (let part of invalidURLParts) {
+        if (request.url.includes(part)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 //Fetch event to fetch from cache first
 self.addEventListener('fetch', async event => {
     event.respondWith((async () => {
         const cache = await caches.open("static");
-        console.log('Service Worker: Fetching from URL: ', event.request.url);
+        //console.log('Service Worker: Fetching from URL: ', event.request.url);
         try {
             let response = await fetch(event.request)
 
             //Don't cache the response if it's not a GET request
-            if (event.request.method === "GET") {
+            if (shouldBeCached(event.request)) {
                 //Cache the fetched response
                 if (response.redirected) {
                     response = await cleanResponse(response);
@@ -64,15 +85,25 @@ self.addEventListener('fetch', async event => {
                 await cache.put(event.request, response.clone());
             }
             return response;
-        }catch(e) {
+        } catch (e) {
             //If the fetch request fails, try to fetch from cache
             const cachedResponse = await cache.match(event.request);
             if (cachedResponse) {
-                console.log('Service Worker: Fetching from Cache: ', event.request.url);
+                //console.log('Service Worker: Fetching from Cache: ', event.request.url);
                 return cachedResponse;
             }
 
-            console.log("Service Worker: Fetch Error: ", event.request.url)
+            //console.log("Service Worker: Fetch Error: ", event.request.url)
+
+            //If we have reached this point, it means that the request is not in the cache
+            //We can return a custom offline page here
+            return new Response("You are offline. Please check your internet connection and try again.", {
+                status: 503,
+                statusText: "Service Unavailable",
+                headers: new Headers({
+                    'Content-Type': 'text/plain'
+                })
+            });
         }
 
     })());
