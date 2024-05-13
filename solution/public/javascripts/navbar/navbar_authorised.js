5e1d7711b71fb93f0ab2f06340e9393e92d6ae6e
xenono <aurbanczyk0@gmail.com>
2024-05-13 15:14:58 +0100

    Update notificaiton counter on reconnect.

--- solution/public/javascripts/navbar/navbar_authorised.js
+++ solution/public/javascripts/navbar/navbar_authorised.js
@@ -1,6 +1,5 @@
 let socket;
 let $notificationCounter;
-
 window.addEventListener("load", async (e) => {
     $notificationCounter = $("#notification-count")
 
@@ -12,7 +11,6 @@ window.addEventListener("load", async (e) => {
         if (countInt > 0) {
             $notificationCounter.removeClass("d-none")
         }
-
         socket.emit("user_active", {user_id: notificationCount.data.user_id})
 
         socket.on("new_notification", () => {
@@ -35,4 +33,14 @@ window.addEventListener("load", async (e) => {
 
 document.addEventListener("DOMContentLoaded", async (e) => {
     socket = io()
+
+    window.addEventListener('online', async (e) => {
+        const notificationCount = await axios.get("/api/new-notification-count")
+        const countInt = parseInt(notificationCount.data.count)
+        $notificationCounter.text(countInt >= 0 ? countInt : 0)
+
+        if (countInt > 0) {
+            $notificationCounter.removeClass("d-none")
+        }
+    });
 });
